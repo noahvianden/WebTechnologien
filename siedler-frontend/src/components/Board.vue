7ef8b131e9405050ec0844381cea55d4def1d14f
noahvianden <noah.vianden@gmail.com>
2024-02-24 19:08:42 +0100

    .

--- siedler-frontend/src/components/Board.vue
+++ siedler-frontend/src/components/Board.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="game-board">
-      <!-- Hexagons für das Spielbrett -->
-      <div
-        v-for="(hexagon, index) in hexagons"
-        :key="index"
-        class="hexagon"
-        :class="hexagon.resource"
-        :style="hexagon.style"
-      >
-        <!-- Anzeige der Ressource oder Nummer (falls vorhanden) -->
-        <span v-if="hexagon.resource !== 'desert'">{{ hexagon.resource }}</span>
-        <span v-else>Wüste</span>
-      </div>
+        <!-- Hexagons für das Spielbrett -->
+        <div
+            v-for="(hexagon, index) in hexagons"
+            :key="index"
+            class="hexagon"
+            :class="hexagon.resource"
+            :style="hexagon.style"
+        >
+            <!-- Anzeige der Ressource oder Nummer (falls vorhanden) -->
+            <span v-if="hexagon.resource !== 'desert'">{{ hexagon.resource }}</span>
+            <span v-else class="desert-tile"></span>
+        </div>
     </div>
-  </template>
+</template>
   
   <script>
   export default {
@@ -22,25 +22,48 @@
       return {
         // Liste der Hexagons mit den verschiedenen Ressourcen und Positionen
         hexagons: [
-          { id: 1, resource: "mountain", style: { top: "100px", left: "200px" } },
-          { id: 2, resource: "pasture", style: { top: "100px", left: "300px" } },
-          { id: 3, resource: "forest", style: { top: "100px", left: "400px" } },
-          { id: 4, resource: "field", style: { top: "200px", left: "150px" } },
-          { id: 5, resource: "hill", style: { top: "200px", left: "250px" } },
-          { id: 6, resource: "pasture", style: { top: "200px", left: "350px" } },
-          { id: 7, resource: "hill", style: { top: "200px", left: "450px" } },
-          { id: 8, resource: "field", style: { top: "300px", left: "100px" } },
-          { id: 9, resource: "forest", style: { top: "300px", left: "200px" } },
-          { id: 10, resource: "desert", style: { top: "300px", left: "300px" } },
-          { id: 11, resource: "forest", style: { top: "300px", left: "400px" } },
-          { id: 12, resource: "mountain", style: { top: "300px", left: "500px" } },
-          { id: 13, resource: "forest", style: { top: "400px", left: "150px" } },
-          { id: 14, resource: "mountain", style: { top: "400px", left: "250px" } },
-          { id: 15, resource: "field", style: { top: "400px", left: "350px" } },
-          { id: 16, resource: "pasture", style: { top: "400px", left: "450px" } },
-          { id: 17, resource: "hill", style: { top: "500px", left: "200px" } },
-          { id: 18, resource: "field", style: { top: "500px", left: "300px" } },
-          { id: 19, resource: "pasture", style: { top: "500px", left: "400px" } },
+          { id: 1, resource: "mountain", style: {   top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) - var(--hexagon-height) * 1.5)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) - var(--hexagon-width))" } },
+          { id: 2, resource: "pasture", style: {    top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) - var(--hexagon-height) * 1.5)", 
+                                                    left: "calc((var(--board-width) + var(--hexagon-width)) / 2)" } },
+          { id: 3, resource: "forest", style: {     top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) - var(--hexagon-height) * 1.5)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) + var(--hexagon-width))" } },
+
+          { id: 4, resource: "field", style: {      top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) - var(--hexagon-height) / 1.335)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) - var(--hexagon-width) * 1.5)" } },
+          { id: 5, resource: "hill", style: {       top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) - var(--hexagon-height) / 1.335)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) - var(--hexagon-width) * 0.5)" } },
+          { id: 6, resource: "pasture", style: {    top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) - var(--hexagon-height) / 1.335)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) + var(--hexagon-width) * 0.5)" } },
+          { id: 7, resource: "hill", style: {       top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) - var(--hexagon-height) / 1.335)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) + var(--hexagon-width) * 1.5)" } },
+
+          { id: 8, resource: "field", style: {      top: "calc((var(--board-height) + var(--hexagon-height)) / 2)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) - var(--hexagon-width) * 2)" } },
+          { id: 9, resource: "forest", style: {     top: "calc((var(--board-height) + var(--hexagon-height)) / 2)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) - var(--hexagon-width))" } },
+          { id: 10, resource: "desert", style: {    top: "calc((var(--board-height) + var(--hexagon-height)) / 2)", 
+                                                    left: "calc((var(--board-width) + var(--hexagon-width)) / 2)" } },
+          { id: 11, resource: "forest", style: {    top: "calc((var(--board-height) + var(--hexagon-height)) / 2)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) + var(--hexagon-width))" } },
+          { id: 12, resource: "mountain", style: {  top: "calc((var(--board-height) + var(--hexagon-height)) / 2)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) + var(--hexagon-width) * 2)" } },
+
+          { id: 13, resource: "forest", style: {    top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) + var(--hexagon-height) / 1.335)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) - var(--hexagon-width) * 1.5)" } },
+          { id: 14, resource: "mountain", style: {  top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) + var(--hexagon-height) / 1.335)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) - var(--hexagon-width) * 0.5)" } },
+          { id: 15, resource: "field", style: {     top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) + var(--hexagon-height) / 1.335)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) + var(--hexagon-width) * 0.5)" } },
+          { id: 16, resource: "pasture", style: {   top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) + var(--hexagon-height) / 1.335)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) + var(--hexagon-width) * 1.5)" } },
+            
+          { id: 17, resource: "hill", style: {   top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) + var(--hexagon-height) * 1.5)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) - var(--hexagon-width))" } },
+          { id: 18, resource: "field", style: {    top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) + var(--hexagon-height) * 1.5)", 
+                                                    left: "calc((var(--board-width) + var(--hexagon-width)) / 2)" } },
+          { id: 19, resource: "pasture", style: {     top: "calc(((var(--board-height) + var(--hexagon-height)) / 2) + var(--hexagon-height) * 1.5)", 
+                                                    left: "calc(((var(--board-width) + var(--hexagon-width)) / 2) + var(--hexagon-width))" } },
         ],
       };
     },
@@ -50,41 +73,60 @@
   <style scoped>
   /* Stil für das Spielbrett */
   .game-board {
-    position: relative;
-    width: 600px;
-    height: 600px;
-  }
+  --board-width: 600px;
+  --board-height: 600px;
+  position: absolute;
+  top: 20%;
+  left: 40%;
+  transform: translate(-50%, -50%);
+  width: var(--board-width);
+  height: var(--board-height);
+}
   
   /* Stil für die Hexagons */
-  .hexagon {
-    width: 100px; /* Verwendung einer CSS-Variable für die Breite */
-    height: 125px; /* Berechnung der Höhe basierend auf der Breite */
-    background-color: #ccc;
-    clip-path: polygon(50% 0, 100% 25%, 100% 75%, 50% 100%, 0 75%, 0 25%);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-  }
-  
-  /* Stil für die verschiedenen Ressourcentypen */
-  .forest {
-    background-color: #228b22;
-  }
-  .pasture {
-    background-color: #90ee90;
-  }
-  .field {
-    background-color: #ffd700;
-  }
-  .hill {
-    background-color: #8b4513;
-  }
-  .mountain {
-    background-color: #808080;
-  }
-  .desert {
-    background-color: #d2b48c;
-  }
-  </style>
-  
+.hexagon {
+  --hexagon-width: 130px; /* Breite eines Hexagons */
+  --hexagon-height: calc(var(--hexagon-width) * 1.1547); /* Höhe eines Hexagons */
+  width: var(--hexagon-width);
+  height: var(--hexagon-height);
+  background-color: #ccc;
+  clip-path: polygon(50% 0, 100% 25%, 100% 75%, 50% 100%, 0 75%, 0 25%);
+  position: absolute;
+}
+
+/* Stil für die verschiedenen Ressourcentypen */
+.mountain {
+  background-image: url('C:/Users/noahv/Downloads/mountain.png');
+  background-size: cover;
+}
+
+.pasture {
+  background-image: url('C:/Users/noahv/Downloads/pasture.png');
+  background-size: cover;
+}
+
+.forest {
+  background-image: url('C:/Users/noahv/Downloads/forest.png');
+  background-size: cover;
+}
+
+.field {
+  background-image: url('C:/Users/noahv/Downloads/field.png');
+  background-size: cover;
+}
+
+.hill {
+  background-image: url('C:/Users/noahv/Downloads/hill.png');
+  background-size: cover;
+}
+
+.desert {
+  background-image: url('C:/Users/noahv/Downloads/desert.png');
+  background-size: cover;
+}
+
+/* Verstecke den Text innerhalb der Hexagons */
+.hexagon span {
+  display: none;
+}
+</style>
